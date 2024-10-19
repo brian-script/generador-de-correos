3df5ed9059f8452f57b4ab88e1d92ecfe83ffa14
brian <brian@kali.brian>
2024-10-19 06:34:59 +0200

    prueba

--- gmail.py
+++ gmail.py
@@ -1,23 +1,34 @@
 import random
 
-print("***generador de correos electronicos ***\n")
+print("*** generador de correos electronicos ***\n")
 
-with open ("palabras.txt", "r") as palabras:
+with open("palabras.txt", "r") as palabras:
     palabra = [linea.strip() for linea in palabras.readlines()]
 
-correo = ""
 lista = []
-cantidad = int(input("Cuantos correos quieres?: "))
+contraseña_final = []
+
+cantidad = int(input("¿Cuántos correos quieres?: "))
 cont = 0
+
 while cont < cantidad:
     correo = ""
-    for i in range(0, 3):
-        correo = correo + random.choice(palabra).strip()
-    lista.append(correo + str(random.randint(1, 1000)) + "@gmail.com")
-    cont = cont + 1
+    caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[{]};:,<.>/?\\|`~ ¬ªº£€¥©®™¿¡"
+    contraseña_inicial = ""
 
-print(lista)
+    for i in range(3):
+        correo += random.choice(palabra).strip()
+    lista.append(correo + str(random.randint(1, 1000)) + "@gmail.com")
+
+    for i in range(10):
+        contraseña_inicial += random.choice(caracteres)
+    contraseña_final.append(contraseña_inicial)  
+
+    cont += 1  
 
 with open("correos.txt", "w") as archivo:
-    for num, a in enumerate(lista):
-        archivo.write(f"\nCorreo numero {num + 1} \n{a}")
+    for i in range(cantidad):
+        archivo.write(f"\nCorreo número {i + 1} \n{lista[i]}")
+        archivo.write(f"\nContraseña: {contraseña_final[i]}\n")
+
+
